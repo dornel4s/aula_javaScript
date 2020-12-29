6b62883b733dbe31ca0e83affe164162ec716491
Dornelas <julianadornelas71@gmail.com>
2020-12-29 11:36:07 -0300

    outro commit

---
+++ js/main.js
@@ -0,0 +1,87 @@
+
+
+/*var validar=0
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+      validar = true
+    }else{
+      validar = false
+    }
+    return validar;
+  }
+
+  var idade = prompt("Qual sua idade");
+  validaIdade(idade);
+  console.log(validar);*/
+
+
+/*function soma(n1, n2){
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japão", "Japão", "Brasil"));*/
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMonth());
+*/
+
+
+/*var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};*/
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++
+};*/
+
+/*var idade = prompt("Qual é a sua idade?");
+if(idade >= 18){
+    alert("você é maior de idade");
+}else{
+    alert("Você é menor de idade");
+};*/
+
+
+
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa" }]
+console.log(frutas);
+alert(frutas[1].nome);
+
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+alert(fruta.cor);*/
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+
+/*var nome = "Jujuju";
+alert("Bem vinda, " + nome);
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"*/
+// alert(nome + " tem " + idade + "anos");
+// alert(idade + idade2);
+/*console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+//alert(frase.replace("Japão", "Brasil"));*/
